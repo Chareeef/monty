a8d5c0c38fbbca9fa0fc8730771030c6980cb4db
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-20 05:14:28 -0700

    modify pall, add nop

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - do nothing
+ * @stack: nothing
+ * @line_number: nothing
+ */
+
+void	nop(__attribute__((unused)) stack_t **stack, unsigned int line_number)
+{
+	(void) line_number;
+}
--- pall.c
+++ pall.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/**
+ * pall - prints all the values on the stack,
+ * starting from the top of the stack.
+ * @stack: head
+ * @line_number: unused
+ */
+
 void	pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t	*head;
