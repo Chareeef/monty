ce0db95e9b961abecc7a4308a50e7411ca2d7899
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-20 11:12:09 -0700

    edit header

--- monty.h
+++ monty.h
@@ -44,5 +44,6 @@ void	pall(stack_t **stack, unsigned int line_number);
 void	nop(stack_t **stack, unsigned int line_number);
 void	push(stack_t **stack, unsigned int line_number);
 void    swap(stack_t **stack, unsigned int line_number);
+void    add(stack_t **stack, unsigned int line_number);
 
 #endif
