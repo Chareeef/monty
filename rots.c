5bfaa4b0468c8f2d7dea782196659be691bdb471
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-22 04:39:50 -0700

    add rotl and rotr

---
+++ rots.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+/**
+ * rotl -  rotates the stack to the top.
+ * @stack: head stack
+ * @line_number: line number
+ */
+
+void	rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t	*current;
+	stack_t	*top;
+
+	if (*stack && (*stack)->next)
+	{
+		top = *stack;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		current = *stack;
+		while (current->next)
+			current = current->next;
+		current->next = top;
+		top->prev = current;
+		top->next = NULL;
+	}
+}
+
+/**
+ * rotr -  rotates the stack to the top.
+ * @stack: head stack
+ * @line_number: line number
+ */
+
+void	rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t	*bottom;
+	stack_t	*current;
+
+	if (*stack && (*stack)->next)
+	{
+		current = (*stack)->next;
+		while (current->next)
+			current = current->next;
+		bottom = current;
+		current = current->prev;
+		current->next = NULL;
+		(*stack)->prev = bottom;
+		bottom->next = *stack;
+		bottom->prev = NULL;
+	}
+}
